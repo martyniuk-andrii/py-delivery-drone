caaa0190ba2687faaec80dfb0e911d2bb29624c2
Andrii Martyniuk <andrijmartunjyk@gmail.com>
2025-02-24 17:58:24 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -7,7 +7,9 @@ class BaseRobot:
     def __init__(self, name: str, weight: int, coords: list = None) -> None:
         self.name = name
         self.weight = weight
-        self.coords = coords if coords is not None else [0, 0]
+        if coords is None:
+            coords = [0, 0]
+        self.coords = coords
 
     def go_forward(self, move: int = 1) -> None:
         self.coords[1] += move
@@ -27,7 +29,9 @@ class BaseRobot:
 
 class FlyingRobot(BaseRobot):
     def __init__(self, name: str, weight: int, coords: list = None) -> None:
-        super().__init__(name, weight, coords=coords or [0, 0, 0])
+        if coords is None:
+            coords = [0, 0, 0]
+        super().__init__(name, weight, coords=coords)
 
     def go_up(self, move: int = 1) -> None:
         self.coords[2] += move
@@ -47,11 +51,21 @@ class DeliveryDrone(FlyingRobot):
     ) -> None:
         super().__init__(name, weight, coords)
         self.max_load_weight = max_load_weight
-        self.current_load = current_load
+        self.current_load = None
+        if current_load is not None:
+            self.hook_load(current_load)
 
     def hook_load(self, cargo: Cargo) -> None:
+        if not isinstance(cargo, Cargo):
+            raise TypeError("The cargo must be an object of class Cargo")
+
         if self.current_load is None and cargo.weight <= self.max_load_weight:
             self.current_load = cargo
+        else:
+            print(
+                f"❌ Unable to lift the load {cargo.weight} kg "
+                f"(max. {self.max_load_weight} kg)"
+            )
 
     def unhook_load(self) -> None:
         self.current_load = None
